9072a1d2627399fcd6574f2cbdf3bcd852fe305b
aolixin <2876589230@qq.com>
2023-08-19 17:35:34 +0800

    Revert "完成phong光照模型"
    This reverts commit 13ecc90d358e137d5e2fdce2dbd1a91ed22c9ae7.

--- global.hpp
+++ global.hpp
@@ -5,10 +5,8 @@
 #ifndef RASTERIZER_GLOBAL_H
 #define RASTERIZER_GLOBAL_H
 
-typedef unsigned char u08;
-#define MY_PI 3.1415926
-#define TWO_PI (2.0* MY_PI)
-
+//#define MY_PI 3.1415926
+//#define TWO_PI (2.0* MY_PI)
 
 
 #endif //RASTERIZER_GLOBAL_H
